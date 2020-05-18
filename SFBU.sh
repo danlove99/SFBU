5499cc73799f05aa9816719c6c0f06a8fe374483
Dan love <danlove99@hotmail.co.uk>
2020-05-19 00:17:28 +0100

    removed commented out code

--- SFBU.sh
+++ SFBU.sh
@@ -4,15 +4,11 @@ if [ ! -f /home/$(whoami)/backups ]; then
 	mkdir /home/$(whoami)/backups
 fi
 
-date +'FORMAT'
- 
-### mm/dd/yyyy ###
-#date +'%m-%d/%Y'
 
-#
-#date +'%r'
- 
 backup_dir=$(date +'%m-%d-%Y')
-echo "Backup dir for today: /home/$(whoami)/backups/${backup_dir}"
 
-tar -czf /home/$(whoami)/backups/"$backupdir".tar.gz $1
+
+tar -czf "/home/$(whoami)/backups/${backup_dir}.tar.gz" -C / $1
+
+echo "Backup dir for today: /home/$(whoami)/backups/${backup_dir}"
+echo "${1}"
