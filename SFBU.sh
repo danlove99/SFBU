fd940456911ba766b348fa5a28ee442f1707cf79
Dan love <danlove99@hotmail.co.uk>
2020-05-19 21:19:33 +0100

    added option for second argument, This argument allows you to specify a directory to store the tar file rather than the fefault 'backup' folder within the user directory

--- SFBU.sh
+++ SFBU.sh
@@ -2,7 +2,14 @@
 
 [[ ! -d /home/$(whoami)/backups ]] && mkdir /home/$(whoami)/backups
 
+if [ -z "$2"]
+then
 tar -czf "/home/$(whoami)/backups/$(date +'%m-%d-%Y').tar.gz" -C / $1
-
 echo "Backing up to: /home/$(whoami)/backups/$(date +'%m-%d-%Y').tar.gz"
+else
+tar -czf $2 -C / $1
+echo "Backing up to: $2"
+fi
+
+
 
