3edd45534843dfd0656ff1302d493675ced12d7d
Dan love <danlove99@hotmail.co.uk>
2020-05-19 14:57:40 +0100

    ...

--- SFBU.sh
+++ SFBU.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [ ! -f /home/$(whoami)/backups ]; then
-	mkdir /home/$(whoami)/backups
-fi
-
+[[ ! -d /home/$(whoami)/backups ]] && mkdir /home/$(whoami)/backups
 
 backup_dir=$(date +'%m-%d-%Y')
 
+tar -czf "/home/$(whoami)/backups/current_backup.tar.gz" -C / $1
 
-tar -czf "/home/$(whoami)/backups/${backup_dir}.tar.gz" -C / $1
-
-echo "Backup dir for today: /home/$(whoami)/backups/${backup_dir}"
-echo "${1}"
+#echo "Backup dir for today: /home/$(whoami)/backups/${backup_dir}"
+#echo "${1}"
